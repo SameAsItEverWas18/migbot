d118fd2a132fef79cc872c666cd572c11b129ff4
smeasitevrws <69230519+SameAsItEverWas18@users.noreply.github.com>
2021-08-11 18:56:44 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import tasks
 import random
 import fnmatch
 client = discord.Client()
+num = 0
 
 @tasks.loop(minutes=20)
 async def sendone():
@@ -11,68 +12,68 @@ async def sendone():
     await channel.send(random.choice(open('migmsg.txt').readlines()))
     await channel2.send(random.choice(open('migmsg.txt').readlines()))
 
-#@tasks.loop(minutes=20)
-#async def sendtwo():
- #   channel = client.get_channel(614877948679553032)
- #   await channel.send(random.choice(open('migmsg.txt').readlines()))
- #   await channel.send(random.choice(open('migmsg.txt').readlines()))
+@tasks.loop(minutes=20)
+async def sendtwo():
+    channel = client.get_channel(614877948679553032)
+    await channel.send(random.choice(open('migmsg.txt').readlines()))
+    await channel.send(random.choice(open('migmsg.txt').readlines()))
 
-#@tasks.loop(minutes=20)
-#async def sendthree():
-#    channel = client.get_channel(614877948679553032)
-#    await channel.send(random.choice(open('migmsg.txt').readlines()))
-#    await channel.send(random.choice(open('migmsg.txt').readlines()))
-#    await channel.send(random.choice(open('migmsg.txt').readlines()))
+@tasks.loop(minutes=20)
+async def sendthree():
+    channel = client.get_channel(614877948679553032)
+    await channel.send(random.choice(open('migmsg.txt').readlines()))
+    await channel.send(random.choice(open('migmsg.txt').readlines()))
+    await channel.send(random.choice(open('migmsg.txt').readlines()))
 
-#@tasks.loop(minutes=20)
-#async def sendfour():
-#    channel = client.get_channel(665397604728832002)
-#    await channel.send(random.choice(open('migmsg.txt').readlines()))
-#    await channel.send(random.choice(open('migmsg.txt').readlines()))
-#    await channel.send(random.choice(open('migmsg.txt').readlines()))
-#    await channel.send(random.choice(open('migmsg.txt').readlines()))
+@tasks.loop(minutes=20)
+async def sendfour():
+    channel = client.get_channel(665397604728832002)
+    await channel.send(random.choice(open('migmsg.txt').readlines()))
+    await channel.send(random.choice(open('migmsg.txt').readlines()))
+    await channel.send(random.choice(open('migmsg.txt').readlines()))
+    await channel.send(random.choice(open('migmsg.txt').readlines()))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-#    if "miguel" in message.content:
-#       await message.channel.send(random.choice(open('migmsg.txt').readlines()))
-
     if "MIGUEL" in message.content:
        await message.channel.send(random.choice(open('migmsg.txt').readlines()))
+       num = random.choice(range(10, 101))
 
     if "mig" in message.content:
        await message.channel.send(random.choice(open('migmsg.txt').readlines()))
+       num = random.choice(range(10, 101))
 
     if "miggy" in message.content:
        await message.channel.send(random.choice(open('migmsg.txt').readlines()))
+       num = random.choice(range(10, 101))
 
     if "kirinvision" in message.content:
        await message.channel.send(random.choice(open('migmsg.txt').readlines()))
+       num = random.choice(range(10, 101))
 
     if "lasvegas63" in message.content:
        await message.channel.send(random.choice(open('migmsg.txt').readlines()))
+       num = random.choice(range(10, 101))
 
-
-num = random.choice(range(10, 101))
-
-if num < 100:
+if num < 85:
     @client.event
     async def on_ready():
         sendone.start()
-#if num < 100 and num > 51:
-#    @client.event
-#    async def on_ready():
-#        sendtwo.start()
-#        print(num)
-#if num < 96 and num > 99:
-#    @client.event
-#    async def on_ready():
-#        sendthree.start()
-#if num == 100:
-#    @client.event
-#    async def on_ready():
-#        sendfour.start()
-client.run('Token')
+if num < 95 and num > 85:
+    @client.event
+    async def on_ready():
+        sendtwo.start()
+        print(num)
+if num < 96 and num > 99:
+    @client.event
+    async def on_ready():
+        sendthree.start()
+if num == 100:
+    @client.event
+    async def on_ready():
+        sendfour.start()
+
+client.run('ODc0ODczMDU4MzM3MzU3ODQ0.YRNTBw.AwNf3o7apZd-braPJLWprLc9yus')
